12b52ff42e4a4dd9aeef7998f9c416575e60f064
Bessie Arino <brarino@gmail.com>
2017-12-18 13:42:55 -0800

    worked mostly on the javascript

--- app.js
+++ app.js
@@ -3,12 +3,28 @@
 var allProducts = [];
 var productNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'sweepers', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
 
+var image1 = document.getElementById('image1');
+var image2 = document.getElementById('image2');
+var image3 = document.getElementById('image3');
+
 function Product(name) {
   this.name = name;
   this.path = 'images/' + this.name + '.jpg';
+  this.displayed = 0;
   this.clicked = 0;
-  this.votes = 0;
+  this.prevDisplayed = false;
   allProducts.push(this);
-}
+};
 
-function createProducts
+function createProducts() {
+  for(var i = 0; i < productNames.length; i++){
+    new Product(productNames[i]);
+  };
+};
+
+var random = function() {
+  return Math.floor(Math.random() * (productNames.length - 0 + 1)) + 0; // via MDN docs
+};
+
+createProducts();
+console.log(allProducts);
